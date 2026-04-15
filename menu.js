ab4857966eb64dac59d89aa02d84da1829dcb8ee
Garifolla Arsen <garifollaarsen5@gmail.com>
2026-04-15 19:03:58 +0600

    Remove Lychee/Jasmine/Mochi, add mojito/siren/kaha photos, remove bgs

--- menu.js
+++ menu.js
@@ -2,7 +2,6 @@ const MENU = {
   tea_cream: {
     title: {ru:'Tea & Cream', kz:'Tea & Cream'},
     items: [
-      {id:'lychee_cream', img:'img/lychee_cream.jpg', name:{ru:'Lychee Cream',kz:'Lychee Cream'}, desc:{ru:'Личи, жасмин, сырная шапка, классическое желе, тапиока',kz:'Личи, жасмин, ірімшік қалпақ, классикалық желе, тапиока'}, sizes:{500:1750,700:1990}, temp:'cold', emoji:'🥤', color:'#ffb8d9'},
       {id:'mango_cream', img:'img/mango_cream.jpg', name:{ru:'Mango Cream Tapioca',kz:'Mango Cream Tapioca'}, desc:{ru:'Манго, сырная шапка, тапиока',kz:'Манго, ірімшік көбігі, тапиока'}, sizes:{500:1750,700:1990}, temp:'cold', emoji:'🥭', color:'#ffd28a'},
       {id:'matcha_peach', img:'img/matcha_peach.jpg', name:{ru:'Matcha Peach Cream',kz:'Matcha Peach Cream'}, desc:{ru:'Персик, зелёный матча, сырная шапка',kz:'Шабдалы, жасыл матча, ірімшік қалпақ, сүт, тапиока'}, sizes:{500:1850,700:2050}, temp:'cold', emoji:'🍑', color:'#c4ecb4'},
       {id:'sunset', img:'img/sunset.jpg', name:{ru:'Sunset',kz:'Sunset'}, desc:{ru:'Клубника, сырная шапка, тапиока',kz:'Жержидек, ірімшік қалпақ, сүт, тапиока'}, sizes:{500:1750,700:1990}, temp:'cold', emoji:'🍓', color:'#ff9fb0'},
@@ -11,14 +10,13 @@ const MENU = {
   classical: {
     title: {ru:'Classical', kz:'Classical'},
     items: [
-      {id:'jasmine', img:'img/jasmine.jpg', name:{ru:'Jasmine Tapioca',kz:'Jasmine Tapioca'}, desc:{ru:'Тапиока, жасминовый чай, молоко',kz:'Тапиока, жасмин шайы, сүт'}, sizes:{500:1650,700:1800}, temp:'cold', emoji:'🍵', color:'#fff5cc'},
       {id:'thai', img:'img/thai.jpg', name:{ru:'Thai Tapioca',kz:'Thai Tapioca'}, desc:{ru:'Тайский изумрудный чай, тапиока, молоко',kz:'Тай зүмірет шайы, тапиока, сүт'}, sizes:{500:1650,700:1800}, temp:'both', emoji:'🧋', color:'#c4ecb4'},
     ]
   },
   lemonade: {
     title: {ru:'Lemonade · 1500 ₸', kz:'Lemonade · 1500 ₸'},
     items: [
-      {id:'mojito', name:{ru:'Mojito Classic',kz:'Mojito Classic'}, desc:{ru:'Классический освежающий мохито',kz:'Классикалық сергітетін мохито'}, sizes:{500:1500}, temp:'cold', emoji:'🍃', color:'#c4ecb4'},
+      {id:'mojito', img:'img/mojito.png', name:{ru:'Mojito Classic',kz:'Mojito Classic'}, desc:{ru:'Классический освежающий мохито',kz:'Классикалық сергітетін мохито'}, sizes:{500:1500}, temp:'cold', emoji:'🍃', color:'#c4ecb4'},
       {id:'marakuya', img:'img/marakuya.png', name:{ru:'Маракуйя',kz:'Маракуйя'}, desc:{ru:'Яркий лимонад с маракуйей',kz:'Маракуйямен жасалған лимонад'}, sizes:{500:1500}, temp:'cold', emoji:'🟡', color:'#ffe08a'},
       {id:'granat', img:'img/granat.png', name:{ru:'Гранатовый удар',kz:'Гранатовый удар'}, desc:{ru:'Насыщенный гранатовый вкус',kz:'Анар дәмі бар қанық лимонад'}, sizes:{500:1500}, temp:'cold', emoji:'🔴', color:'#ff9fb0'},
       {id:'kivi_mar', img:'img/kivi_mar.png', name:{ru:'Киви Маракуйя',kz:'Киви Маракуйя'}, desc:{ru:'Киви и маракуйя',kz:'Киви мен маракуйя'}, sizes:{500:1500}, temp:'cold', emoji:'🥝', color:'#c4ecb4'},
@@ -31,9 +29,9 @@ const MENU = {
   special: {
     title: {ru:'Special', kz:'Special'},
     items: [
-      {id:'siren', img:'img/siren.jpg', name:{ru:'Сиреневое молоко',kz:'Сиреневое молоко'}, desc:{ru:'Клубника, молоко, сырная шапка, голубой курасао',kz:'Құлпынай, сүт, ірімшік қақпағы, көк курасао'}, sizes:{500:1800,700:2000}, temp:'both', emoji:'💜', color:'#d4b8ff'},
+      {id:'siren', img:'img/siren.png', name:{ru:'Сиреневое молоко',kz:'Сиреневое молоко'}, desc:{ru:'Клубника, молоко, сырная шапка, голубой курасао',kz:'Құлпынай, сүт, ірімшік қақпағы, көк курасао'}, sizes:{500:1800,700:2000}, temp:'both', emoji:'💜', color:'#d4b8ff'},
       {id:'cacao_banana', img:'img/cacao_banana.jpg', name:{ru:'Cacao Banana Tapioca',kz:'Cacao Banana Tapioca'}, desc:{ru:'Какао, банан, тапиока, шоколад',kz:'Какао, банан, тапиока, шоколад'}, sizes:{500:1700,700:1900}, temp:'both', emoji:'🍫', color:'#b89070'},
-      {id:'kaha_orange', img:'img/kaha_orange.jpg', name:{ru:'Kaha Orange Ice Tea',kz:'Kaha Orange Ice Tea'}, desc:{ru:'Апельсин, маракуйя, пюре маракуйя, жасмин',kz:'Апельсин, маракуйя, жасмин'}, sizes:{500:1700,700:2000}, temp:'cold', emoji:'🍊', color:'#ffb070'},
+      {id:'kaha_orange', img:'img/kaha_orange.png', name:{ru:'Kaha Orange Ice Tea',kz:'Kaha Orange Ice Tea'}, desc:{ru:'Апельсин, маракуйя, пюре маракуйя, жасмин',kz:'Апельсин, маракуйя, жасмин'}, sizes:{500:1700,700:2000}, temp:'cold', emoji:'🍊', color:'#ffb070'},
       {id:'bubble_coffee', img:'img/bubble_coffee.jpg', name:{ru:'Bubble Coffee',kz:'Bubble Coffee'}, desc:{ru:'Кофе, сырная шапка, тапиока, шоколад',kz:'Кофе, ірімшік көбігі, тапиока, шоколад'}, sizes:{500:1650}, temp:'both', emoji:'☕', color:'#b89070'},
       {id:'milky_matcha', img:'img/milky_matcha.jpg', name:{ru:'Milky & Matcha',kz:'Milky & Matcha'}, desc:{ru:'Зелёный матча, молоко, классическое желе, тапиока',kz:'Жасыл матча, сүт, классикалық желе, тапиока'}, sizes:{500:1650,700:1800}, temp:'cold', emoji:'🍵', color:'#c4ecb4'},
       {id:'snickers', img:'img/snickers.jpg', name:{ru:'Snickers',kz:'Snickers'}, desc:{ru:'Карамель, шоколад, лесные орехи, молоко, тапиока',kz:'Карамель, шоколад, фундук, сүт, тапиока'}, sizes:{500:1850,700:2050}, temp:'cold', emoji:'🍫', color:'#e0b890'},
@@ -53,10 +51,4 @@ const MENU = {
       {id:'palawan', img:'img/palawan.jpg', name:{ru:'Palawan',kz:'Palawan'}, desc:{ru:'Киви, маракуйя, манго, кокосовое желе',kz:'Киви, маракуйя, манго, кокос желесі'}, sizes:{500:1600,700:1850}, temp:'cold', emoji:'🌺', color:'#ffb8d9'},
     ]
   },
-  desserts: {
-    title: {ru:'Десерты', kz:'Десерттер'},
-    items: [
-      {id:'mochi', img:'img/mochi.jpg', name:{ru:'Моти',kz:'Моти'}, desc:{ru:'Ассортимент: киви, малина, вишня, банан, лесные ягоды, нутелла, орео',kz:'Ассортимент: киви, таңқурай, шие, банан, жабайы жидектер, нутелла, орео'}, sizes:{'шт':1200}, temp:'cold', emoji:'🍡', color:'#ffd6ea'},
-    ]
-  }
 };
